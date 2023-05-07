cc6327c1442302d6a7a0ba026a208eb1355376ef
doxas <eizanco@gmail.com>
2023-05-07 18:37:44 +0900

    implement small canvas generation

--- src/canvas.ts
+++ src/canvas.ts
@@ -5,6 +5,9 @@ export class Renderer {
   context: CanvasRenderingContext2D;
   image: HTMLImageElement;
 
+  static get MINIMUM_CELL_WIDTH(): number {return 8;}
+  static get EXPONENT(): number {return 4;}
+
   constructor(parent: HTMLElement) {
     this.parent = parent;
     this.canvas = document.createElement('canvas');
@@ -14,7 +17,7 @@ export class Renderer {
     this.eventSetting();
   }
 
-  eventSetting() {
+  eventSetting(): void {
     const body = document.body;
     body.addEventListener('dragover', (evt) => {
       evt.preventDefault();
@@ -38,12 +41,31 @@ export class Renderer {
     }, false);
   }
 
-  render() {
+  render(): void {
     const c = this.canvas;
     const cx = this.context;
     c.width = this.image.naturalWidth;
     c.height = this.image.naturalHeight;
     cx.clearRect(0, 0, c.width, c.height);
-    cx.drawImage(this.image, 0, 0, c.width, c.height);
+    const smallData = this.generateSmallCanvas();
+    const cellData = this.generateCell(smallData);
+  }
+
+  generateSmallCanvas(): ImageData {
+    const width = this.canvas.width - this.canvas.width % Renderer.MINIMUM_CELL_WIDTH;
+    const height = this.canvas.height - this.canvas.height % Renderer.MINIMUM_CELL_WIDTH;
+    const dw = width / Renderer.MINIMUM_CELL_WIDTH;
+    const dh = height / Renderer.MINIMUM_CELL_WIDTH;
+    const c = document.createElement('canvas');
+    const cx = c.getContext('2d');
+    c.width = dw;
+    c.height = dh;
+    cx.drawImage(this.image, 0, 0, this.canvas.width, this.canvas.height, 0, 0, dw, dh);
+    const data = cx.getImageData(0, 0, dw, dh);
+    return data;
+  }
+
+  generateCell(data: ImageData): ImageData {
+
   }
 }
